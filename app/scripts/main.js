4430dacdd39de8e46763f62d71d48e01bd5fddc9
jhath16 <hathaway.justin@gmail.com>
2014-09-16 19:11:20 -0400

    GET works for issues on page load

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('The Iron Yard Rocks');
+function renderTemplate(templateId, container, model) {
+  var templateString = $('#' + templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderedTemplate = templateFunction(model);
+  $(container).append(renderedTemplate);
+}
+
+/*  $.getJSON("https://api.github.com/issues").done(function(data) {
+  console.log(data);
+  //renderTemplate('title','.title-holder',data);
+  $.each(data, function(i) {
+    $.getJSON(data[i].comments_url).done(function(comments) {
+      if (comments.length != 0) {
+        console.log(comments);
+        renderTemplate('comment', '.comment-holder', comments[0]);
+      };
+    })
+  });
+});  */
+
+
+$.getJSON("https://api.github.com/issues").done(function(data) {
+  console.log(data);
+  $.each(data, function(i) {
+    console.log(data[i]);
+    renderTemplate('title','.title',data[i]);
+  })
+});
