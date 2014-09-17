28756116ba415d6c3aac1b6f5109d14cd91bd542
jhath16 <hathaway.justin@gmail.com>
2014-09-17 02:30:09 -0400

    titles are loaded in real time

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,24 +5,33 @@ function renderTemplate(templateId, container, model) {
   $(container).append(renderedTemplate);
 }
 
-/*  $.getJSON("https://api.github.com/issues").done(function(data) {
-  console.log(data);
-  //renderTemplate('title','.title-holder',data);
-  $.each(data, function(i) {
-    $.getJSON(data[i].comments_url).done(function(comments) {
-      if (comments.length != 0) {
-        console.log(comments);
-        renderTemplate('comment', '.comment-holder', comments[0]);
-      };
-    })
-  });
-});  */
+function htmlTemplate(templateId, container, model) {
+  var templateString = $('#' + templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderedTemplate = templateFunction(model);
+  $(container).html(renderedTemplate);
+}
+
+var url = "https://api.github.com/issues";
 
 
-$.getJSON("https://api.github.com/issues").done(function(data) {
-  console.log(data);
+//Clears the title div and loads all current issues' titles
+var grabTitles = function() {
+  console.log('clearing');
+  $('.title').empty();
+$.getJSON(url).done(function(data) {
   $.each(data, function(i) {
-    console.log(data[i]);
     renderTemplate('title','.title',data[i]);
-  })
+    console.log('appended new title');
+  });
+});
+};
+setInterval(grabTitles,3000);
+
+
+$(document).on('click','.button', function() {
+  var id = $(this).attr('data-id');
+  $.getJSON(url).done(function(data) {
+    console.log(id);
+  });
 });
