b0965486328b064213fde937ccc4d9a68dbffc6f
jhath16 <hathaway.justin@gmail.com>
2014-09-17 03:54:27 -0400

    added notes on function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,35 +19,35 @@ var url = "https://api.github.com/issues";
 var grabTitles = function() {
   console.log('clearing');
   $('.title').empty();
-$.getJSON(url).done(function(data) {
-  $.each(data, function(i) {
-    renderTemplate('title','.title',data[i]);
-    console.log('appended new title');
+  $.getJSON(url).done(function(data) {
+    $.each(data, function(i) {
+      renderTemplate('title', '.title', data[i]);
+      console.log('appended new title');
+    });
   });
-});
 };
 
 //Load on page load
 grabTitles();
 
 //Set interval for real time pulling
-//setInterval(grabTitles,5000);
+setInterval(grabTitles, 5000);
 
 
 //Finds and prints comments on click event
-$(document).on('click','.button', function() {
-  $('.comment').empty();
-  var id = $(this).attr('data-id');
-  $.getJSON(url).done(function(data) {
-    $.each(data, function(i) {
-      //console.log(data[i].id);
-      if(id == data[i].id) {
-        $.getJSON(data[i].comments_url).done(function(commentObject) {
+$(document).on('click', '.button', function() {
+  $('.comment').empty(); //empty the contents of the comment box
+  var id = $(this).attr('data-id'); //set the var id to id of button clicked
+  $.getJSON(url).done(function(data) { //fetch data of all issues
+    $.each(data, function(i) { //go through each of the issue objects
+      if (id == data[i].id) {//match the clicked id to the corresponding object
+        $.getJSON(data[i].comments_url).done(function(commentObject) { //fetch the comments object of the matched object
           console.log(commentObject);
-            $.each(commentObject, function(c) {
-              console.log(commentObject[c]);
-              renderTemplate('comment','.comment',commentObject[c]);
-            });
+          $.each(commentObject, function(c) { //go through each of the comments
+            console.log(commentObject[c]);
+            renderTemplate('comment', '.comment', commentObject[c]);
+            //render each of the comments to the template
+          });
         });
       }
     });
